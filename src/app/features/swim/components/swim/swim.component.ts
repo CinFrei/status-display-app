8ccbbbe45b2b33cbfa9b858d7eb2474722403861
CinFrei <dev@cinfrei.de>
2024-11-22 17:31:56 +0100

    add feature swim.c

--- src/app/features/swim/components/swim/swim.component.ts
+++ src/app/features/swim/components/swim/swim.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { SwimService } from '../../services/swim.service';
+import { Subscription } from 'rxjs';
+import { TimeService } from '../../../../core/services/time.service';
+import { HalfCardComponent } from "../../../../shared/components/half-card/half-card.component";
+import { HalfCardData } from '../../../../shared/components/half-card/half-card.component';
 
 @Component({
   selector: 'app-swim',
-  imports: [],
+  imports: [HalfCardComponent],
   templateUrl: './swim.component.html',
-  styleUrl: './swim.component.scss'
+  styleUrl: './swim.component.scss',
 })
 export class SwimComponent {
+  //Vars
+  public swimClubTime: string = ''; // Hier speichern wir die Schwimmzeit
+  private subscriptions: Subscription[] = [];
 
+  swimHalfCardData: HalfCardData = {
+    icon: 'tsunami',
+    contentStrong: '',
+    content1: this.swimClubTime,
+    content2: 'blub',
+  };
+
+  constructor(
+    private swimService: SwimService,
+    private timeService: TimeService,
+  ) { }
+
+  ngOnInit(): void {
+    const weekDay = this.timeService.getCurrentWeekDay(); // Holt den Wochentag
+    // Hole die Schwimmclub-Zeiten und zeige sie für den aktuellen Tag
+    this.swimService.getSwimClubTimes().subscribe((jData) => {
+      // Hole die Zeit für den aktuellen Tag
+      const newSwimTime = jData['swim-club-week'][weekDay];
+      if (this.swimClubTime !== newSwimTime) {
+        this.swimClubTime = newSwimTime; // Nur wenn sich die Zeit geändert hat
+        this.swimHalfCardData.content1 = newSwimTime; // Update nur dann, wenn notwendig
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    // Kündige alle Subscriptions
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
 }
--- src/app/features/swim/services/swim.service.ts
+++ src/app/features/swim/services/swim.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'; // Erlaubt uns, diesen Service in der App zu verwenden
+import { HttpClient } from '@angular/common/http'; // Macht HTTP-Anfragen möglich
+import { Observable } from 'rxjs'; // Typ für den Datenstrom (Observable)
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' // Macht den Service überall verfügbar, ohne ihn manuell zu registrieren
 })
 export class SwimService {
+  constructor(private http: HttpClient) { }   // `http` ist eine Instanz von HttpClient, mit der wir HTTP-Anfragen machen können.
 
-  constructor() { }
+
+  // Methode lädt die SchwimmClubZeiten
+  getSwimClubTimes(): Observable<any> {
+    // Wir nutzen HTTP, um die JSON-Datei zu laden.
+    // Das Ergebnis ist ein Observable, das die Daten enthält.
+    return this.http.get('assets/swim-club-times.json');  // Pfad zur JSON-Datei
+  }
 }
