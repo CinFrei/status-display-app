5363dd9ad9fead4a031e1bb96df1381cc57c31fe
CinFrei <dev@cinfrei.de>
2024-12-13 09:17:46 +0100

    start calendar.feature

--- src/app/features/calendar/components/calendar/calendar.component.ts
+++ src/app/features/calendar/components/calendar/calendar.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CalendarService } from '../../services/calendar.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-calendar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
 export class CalendarComponent {
+  events: any[] = [];
 
+  constructor(private calendarService: CalendarService) { }
+
+  ngOnInit() {
+    this.calendarService.getCalendarEvents().subscribe({
+      next: (data) => (this.events = data),
+      error: (err) => console.error('Fehler beim Abrufen der Daten', err),
+    });
+  }
 }
--- src/app/features/calendar/services/calendar.service.ts
+++ src/app/features/calendar/services/calendar.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CalendarService {
+  private apiUrl = 'http://localhost:3000/api/calendar'; // Backend-URL
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCalendarEvents(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
 }
